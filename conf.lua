5794acf1b9602d82943ca7317196db9866b4f302
Mohammed <33269810+MoGaming@users.noreply.github.com>
2024-08-30 18:37:31 -0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 -- default configure file for love2d, it gets replaced by love.window.setMode(..) function
 
 function love.conf(t)
-	t.window.vsync = false
+  t.window.vsync = false
   t.window.depth = 24
   t.window.stencil = 8
   t.window.width = 1200
